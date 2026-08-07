5e6571a304087a3ff4e068b59403f6fefb31f582
Kernux <kamronsaydullaev09@gmail.com>
2026-08-07 09:25:46 +0500

    Ping

--- script.sh
+++ script.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 echo "CI/CD ishladi !" > natija.txt
 echo "File yaratildi: $(date)" >> natija.txt
-sudo install nmap -y
-nmap 192.178.183.138 > nmap.txt
+ping -c 4 google.com > ping.txt
